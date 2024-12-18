239ac53a9c86e12e538052c4a14b898347ce2516
ogawayuto <ogaoga11271127@gmail.com>
2024-12-18 21:56:07 +0900

    mod

--- zsh/style.zsh
+++ zsh/style.zsh
@@ -2,6 +2,6 @@ autoload -U colors; colors
 
 zstyle ':completion:*' list-colors 'di=36' 'ex=31' 'ln=35'
 
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh -s -- --yes
 eval "$(starship init zsh)"
 starship preset bracketed-segments -o ~/starship.toml
